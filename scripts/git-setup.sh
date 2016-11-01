2950a1ff152b28058f1a662949609d870e96a66b
Edward Cragg <edward.cragg@codethink.co.uk>
2016-11-01 13:54:08 +0000

    Add git SMTP setup
    Set up SMTP settings for git send-email

--- scripts/git-setup.sh
+++ scripts/git-setup.sh
@@ -6,3 +6,11 @@ git config --global alias.st status
 git config --global alias.co checkout
 
 git config --global core.editor vim
+
+# SMTP support
+read -p 'SMTP username? ' username
+read -p 'SMTP server? ' server
+git config --global sendemail.smtpserver $server
+git config --global sendemail.smtpserverport 587
+git config --global sendemail.smtpuser $username
+git config --global sendemail.smtpencryption tls
